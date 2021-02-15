c748dd7c0c07d778641f1b7112848a3b0c1561c8
Carl <carl.agbenyega@gmail.com>
2021-02-14 21:32:59 -0700

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c585c9de8c5294773be7ab081f636b80'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a3f8abc2-4e9d-4495-8021-86053b74b407'
+};
